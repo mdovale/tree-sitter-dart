3402c8566b19162f92e5c7d94ed61cf8e3740586
Miguel Dovale <mikedovale@pm.me>
2026-04-14 16:27:21 -0700

    Slates: Swift PM test target, SwiftTreeSitter, exclude noise for Xcode

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
 // swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
@@ -6,42 +7,68 @@ let package = Package(
     products: [
         .library(name: "TreeSitterDart", targets: ["TreeSitterDart"]),
     ],
-    dependencies: [],
-    targets: [
-        .target(name: "TreeSitterDart",
-                path: ".",
-                exclude: [
-                    "Cargo.toml",
-                    "Makefile",
-                    "binding.gyp",
-                    "bindings/c",
-                    "bindings/go",
-                    "bindings/node",
-                    "bindings/python",
-                    "bindings/rust",
-                    "prebuilds",
-                    "grammar.js",
-                    "package.json",
-                    "package-lock.json",
-                    "pyproject.toml",
-                    "setup.py",
-                    "test",
-                    "examples",
-                    ".editorconfig",
-                    ".github",
-                    ".gitignore",
-                    ".gitattributes",
-                    ".gitmodules",
-                ],
-                sources: [
-                    "src/parser.c",
-                    "src/scanner.c"
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
+    dependencies: [
+        .package(name: "SwiftTreeSitter", url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.9.0"),
     ],
-    cLanguageStandard: .c11
+    targets: [
+        .target(
+            name: "TreeSitterDart",
+            dependencies: [],
+            path: ".",
+            exclude: [
+                "binding.gyp",
+                "bindings/c",
+                "bindings/go",
+                "bindings/node",
+                "bindings/python",
+                "bindings/rust",
+                "bindings/swift/TreeSitterDartTests",
+                "Cargo.toml",
+                "Cargo.lock",
+                "Dart.g",
+                "Makefile",
+                "grammar.js",
+                "LICENSE",
+                "README.md",
+                "assets",
+                "go.mod",
+                "package.json",
+                "pubspec.yaml",
+                "pyproject.toml",
+                "setup.py",
+                "src/grammar.json",
+                "src/node-types.json",
+                "test",
+                "tester",
+                "test_all.sh",
+                "testitem.html",
+                "tree_sitter",
+                "tree-sitter-dart.wasm",
+                "tree-sitter.json",
+                ".editorconfig",
+                ".gitattributes",
+                ".github",
+                ".gitignore",
+                ".gitmodules",
+            ],
+            sources: [
+                "src/parser.c",
+                "src/scanner.c",
+            ],
+            resources: [
+                .copy("queries"),
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")],
+        ),
+        .testTarget(
+            name: "TreeSitterDartTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterDart",
+            ],
+            path: "bindings/swift/TreeSitterDartTests",
+        ),
+    ],
+    cLanguageStandard: .c11,
 )
